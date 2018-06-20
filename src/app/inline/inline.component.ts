469d932f399e961869fb8b1bc4d06352ed271336
subodh <subodhgawade123@gmail.com>
2018-06-20 09:29:45 +0530

    First Assignment for Angular 6  webdevelopment

---
+++ src/app/inline/inline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inline',
+  template: `<h1>Hello Inline Marvellous Infosystem<h1>`,
+  styleUrls: ['./inline.component.css']
+})
+export class InlineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
